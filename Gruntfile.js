3b4d8176c6268e8900c38b8509f385e99080a5b8
Tushar Mathur <tusharmath@gmail.com>
2013-05-12 16:25:33 +0530

    fixed  #2 Grunt string-replace task for manifest.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,26 +18,6 @@ var _config = {
 			npm: false
 		}
 	},
-	'string-replace': {
-		manifest: {
-			files: [{
-				'./src/manifest.json': './src/manifest.template.json'
-			}],
-
-			options: {
-				replacements: [{
-					pattern: /\!\{version\}/g,
-					replacement: '<%= pkg.version %>'
-				}, {
-					pattern: /\!\{name\}/g,
-					replacement: '<%= pkg.name %>'
-				}, {
-					pattern: /\!\{description\}/g,
-					replacement: '<%= pkg.description %>'
-				}]
-			}
-		}
-	},
 	uglify: {
 		dist: {
 			files: {
@@ -45,12 +25,22 @@ var _config = {
 				'bin/src/site.min.js': ['src/popup/*.js']
 			}
 		}
+	},
+	manifest_merge: {
+		foo: {
+			pkg: './',
+			src: './src',
+			dest: './bin/src'
+		}
+
 	}
 };
 
 module.exports = function(grunt) {
 	grunt.initConfig(_config);
 	_config.pkg = grunt.file.readJSON('package.json');
+
+
 	grunt.loadNpmTasks('grunt-zip');
 	grunt.loadNpmTasks('grunt-release');
 	//grunt.loadNpmTasks('grunt-contrib-copy');
@@ -58,5 +48,23 @@ module.exports = function(grunt) {
 	//grunt.loadNpmTasks('grunt-contrib-nodeunit');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-string-replace');
-	grunt.registerTask('publish', ['release', 'string-replace', 'zip']);
+
+	grunt.registerMultiTask('manifest_merge', 'Log stuff.', function() {
+		var pkg = this.data.pkg + '/package.json';
+		var src = this.data.src + '/manifest.json';
+		var dest = this.data.dest + '/manifest.json';
+
+		var pkgContent = grunt.file.readJSON(pkg);
+		var srcContent = grunt.file.readJSON(src);
+
+		srcContent.name = pkgContent.name;
+		srcContent.version = pkgContent.version;
+		srcContent.description = pkgContent.description;
+		srcContent.background.scripts = ['bg.min.js'];
+		grunt.file.write(dest, JSON.stringify(srcContent));
+
+	});
+
+
+	//grunt.registerTask('publish', ['string-replace', 'zip']);
 };
